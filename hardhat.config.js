d7c64fd10aa1a6c0649c73aa09071f01f81c9fb6
Shashank K Prabhu <shashankkp02@gmail.com>
2023-11-10 01:59:18 +0530

    Changes aapp addres to invisible

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,14 @@
 require("dotenv").config();
 require("@nomiclabs/hardhat-ethers");
 const { PRIVATE_KEY } = process.env;
+const APP_ADDRESS = process.env.APP_ADDRESS;
 module.exports = {
   solidity: "0.8.9",
   defaultNetwork: "sepolia",
   networks: {
     hardhat: {},
     sepolia: {
-      url: "https://eth-sepolia.g.alchemy.com/v2/x1yrkWuu8EmTtaGqti9QgiDnG9KuI78T",
+      url: APP_ADDRESS,
       accounts: [`0x${PRIVATE_KEY}`],
     },
   },
